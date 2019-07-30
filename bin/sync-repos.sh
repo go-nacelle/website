4ee25764fa6879fcbbd752f8d222485d18b95499
Eric Fritz <eric@eric-fritz.com>
2019-07-29 21:04:59 -0700

    Update sync repos script to take repo name.

--- bin/sync-repos.sh
+++ bin/sync-repos.sh
@@ -1,25 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+declare -A repos
+repos["nacelle"]="./content/docs/core/overview.md"
+repos["config"]="./content/docs/core/config.md"
+repos["log"]="./content/docs/core/log.md"
+repos["process"]="./content/docs/core/process.md"
+repos["service"]="./content/docs/core/service.md"
+repos["grpcbase"]="./content/docs/base processes/grpcbase.md"
+repos["httpbase"]="./content/docs/base processes/httpbase.md"
+repos["lambdabase"]="./content/docs/base processes/lambdabase.md"
+repos["workerbase"]="./content/docs/base processes/workerbase.md"
+repos["awsutil"]="./content/docs/libraries/awsutil.md"
+repos["pgutil"]="./content/docs/libraries/pgutil.md"
+repos["chevron"]="./content/docs/frameworks/chevron.md"
+repos["scarf"]="./content/docs/frameworks/scarf.md"
 
 function add_content() {
+    echo "Updating $2"
     header=`cat "${2}" | sed '/<!-- Fold -->/q'`
     content=`curl -s "https://raw.githubusercontent.com/go-nacelle/${1}/master/README.md" | sed '1,/---/d'`
     echo -e "${header}\n${content}" > "${2}"
 }
 
-add_content "nacelle" "./content/docs/core/overview.md"
+repo=${1:-"all"}
+target=${repos[$repo]}
 
-for repo in config log process service; do
-    add_content "${repo}" "./content/docs/core/${repo}.md"
-done
+if [ $repo == "all" ]; then
+    for repo in ${!repos[@]}; do
+        add_content $repo "${repos[$repo]}"
+    done
+else
+    if [ -z "$target" ]; then
+        echo "Unknown repo $repo"
+        exit 1
+    fi
 
-for repo in grpcbase httpbase lambdabase workerbase; do
-    add_content "${repo}" "./content/docs/base processes/${repo}.md"
-done
-
-for repo in awsutil pgutil; do
-    add_content "${repo}" "./content/docs/libraries/${repo}.md"
-done
-
-for repo in chevron scarf; do
-    add_content "${repo}" "./content/docs/frameworks/${repo}.md"
-done
+    add_content $repo "$target"
+fi
